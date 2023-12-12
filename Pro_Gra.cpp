c66024b47a3fd29062355e2e2ba6821e5a7ed933
ZigZap2137 <146915825+ZigZap2137@users.noreply.github.com>
2023-12-12 21:59:06 +0100

    Add files via upload
    Dodanie Klas, przeniesienie pętli gry poza funkcje main().

--- Pro_Gra.cpp
+++ Pro_Gra.cpp
@@ -1,38 +1,113 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
 #include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
 #include <iostream>
 
+class Game
+{
+private:
+	sf::RenderWindow* window;
+	sf::VideoMode videomode;
+	sf::Event ev;
+
+	void iniVar();//zmiennych
+	void iniWin();//okna
+public:
+	//konstruktory
+	Game();
+	virtual ~Game();
+
+	//dostepy(otwieranie okna)
+	const bool running() const;
+
+	//funkcje
+	void pollEvents();
+	void update();
+	void render();
+};
+
+const bool Game::running() const
+{
+	return this->window->isOpen();
+}
+
+//konstruktory
+void Game::iniVar()
+{
+	this->window = nullptr;
+}
+
+void Game::iniWin()
+{
+	this->videomode.height = 600;
+	this->videomode.width = 800;
+
+	this->window = new sf::RenderWindow(sf::VideoMode(800, 600), "SFML animation", sf::Style::Default);
+}
+
+Game::Game()
+{
+	//taka kolejnoœæ bo na pocz¹tku chcemy pusty wskaŸnik
+	this->iniVar();
+	this->iniWin();
+}
+
+Game::~Game()
+{
+	delete this->window;
+}
+
+void Game::pollEvents()
+{
+	//event polling
+	while (this->window->pollEvent(this->ev))
+	{
+		switch (this->ev.type)
+		{
+		case sf::Event::Closed:
+			this->window->close();
+			break;
+		case sf::Event::KeyPressed:
+			if (ev.key.code == sf::Keyboard::Escape)
+				this->window->close();
+			break;
+
+		}
+	}
+}
+void Game::update()
+{
+	this->pollEvents();
+}
+
+void Game::render()
+{
+	this->window->clear(sf::Color::Red);
+
+	//rysowanie obiektow
+	this->window->display();
+}
 
 int main()
 {
-	sf::VideoMode vm(800, 600);
-	sf::RenderWindow window(vm, "SFML animation", sf::Style::Default);
-	sf::Event event;
+	//init Game engine
+	Game game;
 
-	while (window.isOpen())
+	//Petla gry
+	while (game.running())
 	{
-		while (window.pollEvent(event))
-		{
-			switch (event.type)
-			{
-			case sf::Event::Closed:
-					window.close();
-					break;
-			case sf::Event::KeyPressed:
-				if (event.key.code == sf::Keyboard::Escape)
-					window.close();
-				break;
+		//update
+		game.update();
 
-			}
-		}		
+		//render
+		game.render();
+
 	}
-
-	//update
-	//10.12.23 okno programu
-
-	//render
-	window.clear(sf::Color(200,200,200,200));
-
-	window.display();
+	//koniec gry
+	return 0;
 }
+//aktualnosci
+//10.12.23 okno programu
+//12.12.23 dodanie klas i przeniesienie pêtli gry poza funkcje main
